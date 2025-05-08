1ce36910366c0d3c611c8fa0950e50e7471b2765
Zakaria Akrach <zakrach29@gmail.com>
2025-05-08 23:40:12 +0200

    try for railway

--- src/main/java/com/zakaria/streamingPlatform/StreamingPlatformApplication.java
+++ src/main/java/com/zakaria/streamingPlatform/StreamingPlatformApplication.java
@@ -2,8 +2,10 @@ package com.zakaria.streamingPlatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({ "com.zakaria.streamingPlatform.*" })
 public class StreamingPlatformApplication {
 
 	public static void main(String[] args) {
