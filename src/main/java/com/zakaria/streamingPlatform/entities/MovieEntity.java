c15f0af303c276456bdaf196f9b1feda2de0e846
Zakaria Akrach <zakrach29@gmail.com>
2025-05-10 21:48:56 +0200

    import movie from TMDB api

---
+++ src/main/java/com/zakaria/streamingPlatform/entities/MovieEntity.java
@@ -0,0 +1,116 @@
+package com.zakaria.streamingPlatform.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "movie")
+public class MovieEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private TypeMovie typeMovie;
+    private String language;
+    private String title;
+    @Lob
+    private String description;
+    private String posterPath; //img piccolo
+    private String backdropPath; //img grande
+    private LocalDate releaseDate;
+    private boolean active;
+    private LocalDate dateCreated;
+
+    public MovieEntity() {
+    }
+
+    public MovieEntity(Long id, TypeMovie typeMovie, String language, String title, String description, String posterPath,
+                       String backdropPath, LocalDate releaseDate, boolean active, LocalDate dateCreated) {
+        this.id = id;
+        this.typeMovie = typeMovie;
+        this.language = language;
+        this.title = title;
+        this.description = description;
+        this.posterPath = posterPath;
+        this.backdropPath = backdropPath;
+        this.releaseDate = releaseDate;
+        this.active = active;
+        this.dateCreated = dateCreated;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public TypeMovie getTypeMovie() {
+        return typeMovie;
+    }
+
+    public void setTypeMovie(TypeMovie typeMovie) {
+        this.typeMovie = typeMovie;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public LocalDate getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDate dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+}
