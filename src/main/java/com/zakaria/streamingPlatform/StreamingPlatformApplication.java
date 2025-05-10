638993b0ddb53201f1898950e56a9086b16e7ba6
Zakaria Akrach <zakrach29@gmail.com>
2025-05-10 05:11:00 +0200

    secure my properties with variable
    add dependency dotenv (.env to properties)

--- src/main/java/com/zakaria/streamingPlatform/StreamingPlatformApplication.java
+++ src/main/java/com/zakaria/streamingPlatform/StreamingPlatformApplication.java
@@ -2,14 +2,12 @@ package com.zakaria.streamingPlatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({ "com.zakaria.streamingPlatform.*" })
 public class StreamingPlatformApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StreamingPlatformApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StreamingPlatformApplication.class, args);
+    }
 
 }
