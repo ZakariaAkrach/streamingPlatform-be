1444963ca03de6fbb014a06b8be603600ea9b337
Zakaria Akrach <zakrach29@gmail.com>
2025-06-11 23:17:57 +0200

    FIX auth role
    I add ROLE_ since spring boot security read before role ROLE_
    @EnableMethodSecurity -> to enable hasRole into controller

--- src/main/java/com/zakaria/streamingPlatform/StreamingPlatformApplication.java
+++ src/main/java/com/zakaria/streamingPlatform/StreamingPlatformApplication.java
@@ -3,9 +3,11 @@ package com.zakaria.streamingPlatform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
 @EnableCaching
+@EnableMethodSecurity
 public class StreamingPlatformApplication {
 
     public static void main(String[] args) {
