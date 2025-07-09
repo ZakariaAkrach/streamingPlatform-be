afce715400a8a4081bcadc7f70b232640bf16a21
Zakaria Akrach <zakrach29@gmail.com>
2025-07-09 22:05:51 +0200

    FIX add equals and hashCode
    I add them because the composed key give me some error in the console

--- src/main/java/com/zakaria/streamingPlatform/compositeKeys/UserCommentKey.java
+++ src/main/java/com/zakaria/streamingPlatform/compositeKeys/UserCommentKey.java
@@ -3,11 +3,25 @@ package com.zakaria.streamingPlatform.compositeKeys;
 import jakarta.persistence.Embeddable;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
 public class UserCommentKey implements Serializable {
     private Long userId;
     private Long commentId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserCommentKey that = (UserCommentKey) o;
+        return Objects.equals(userId, that.userId) && Objects.equals(commentId, that.commentId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, commentId);
+    }
 
     public Long getUserId() {
         return userId;
--- src/main/java/com/zakaria/streamingPlatform/compositeKeys/UserMovieKey.java
+++ src/main/java/com/zakaria/streamingPlatform/compositeKeys/UserMovieKey.java
@@ -2,10 +2,25 @@ package com.zakaria.streamingPlatform.compositeKeys;
 
 import jakarta.persistence.Embeddable;
 
+import java.util.Objects;
+
 @Embeddable
 public class UserMovieKey {
     private Long userId;
     private Long movieId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserMovieKey that = (UserMovieKey) o;
+        return Objects.equals(userId, that.userId) && Objects.equals(movieId, that.movieId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, movieId);
+    }
 
     public Long getUserId() {
         return userId;
